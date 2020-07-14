c5832cf005a06b7e7a582c722d4be9da72520580
rrahn <rene.rahn@fu-berlin.de>
2020-07-14 17:29:57 +0200

    [MISC] Cleanup the epr rank support.

--- include/sdsl/rank_support_int_v.hpp
+++ include/sdsl/rank_support_int_v.hpp
@@ -77,6 +77,11 @@ public:
     {
         return &word;
     }
+
+    constexpr operator uint64_t() const noexcept
+    {
+        return word;
+    }
 };
 
 //! A rank structure proposed by Christopher Pockrandt
@@ -98,7 +103,6 @@ public:
 template <uint8_t alphabet_size, uint8_t vector_width=0, uint8_t words_per_block = 1, uint8_t blocks_per_superblock = 4>
 class rank_support_int_v final : public rank_support_int<alphabet_size,vector_width> {
 public:
-	// typedef int_vector<vector_width> int_vector_type;
 	typedef typename rank_support_int<alphabet_size,vector_width>::size_type size_type;
 	typedef typename rank_support_int<alphabet_size,vector_width>::value_type value_type;
 
@@ -106,9 +110,6 @@ public:
 	using rank_support_int<alphabet_size,vector_width>::sigma_bits;
 
 public:
-	// int_vector<0> m_block;
-	// int_vector<64> m_superblock;
-
 	static constexpr uint64_t values_per_word{64ULL / sigma_bits};
 	static constexpr uint32_t values_per_block{words_per_block * values_per_word};
     static constexpr uint64_t words_per_superblock{words_per_block * blocks_per_superblock};
@@ -138,9 +139,6 @@ public:
 		}
 
 		constexpr uint64_t new_width{ceil_log2(values_per_superblock)};
-		// std::cout << new_width << '\n';
-		// m_block.width(new_width);
-		// m_superblock.width(64);
 
 		// NOTE: number of elements is artificially increased by one because rank can be called on m_v[size()]
 		uint64_t const word_count = ((this->m_v->size() - 1 + 1) / values_per_word) + 1; // equivalent to ceil(m_v->size() / values_per_word)
@@ -154,101 +152,9 @@ public:
 		size_type const superblock_count = (word_count + words_per_superblock - 1) / words_per_superblock;
 		size_type const superblock_size = superblock_count * max_letter; // equivalent to ceil(word_count / words_per_superblock) * max_letter
 
-        //----------------------------------------------------------------------
-        // Old initialisation
-        //----------------------------------------------------------------------
-
-        // m_block.resize(block_size);
-		// m_superblock.resize(superblock_size, 0);
-
-		// m_compressed_text_per_superblock.resize(superblock_count);  // Store the text only once. We will store for every superblock the text in a vector.
-
 		uint64_t const * data = this->m_v->data();
 		std::array<uint64_t, max_letter> buf_blocks{};
 		std::array<uint64_t, max_letter> buf_superblocks{};
-
-		// Why are only the first max_letter many super blocks set to 0?
-		// We could directly initialise the super blocks with 0 in the resize?
-		// for (uint64_t v = 0; v < max_letter; ++v)
-		// 	m_superblock[v] = 0;
-
-		// Precompute blocks and superblocks
-		// NOTE: divisors in modulo operations are constexpr and hence are expected to be cheap
-		// auto text_it = v->begin();
-		// for (uint64_t word_id = 0, block_id = 0, superblock_id = max_letter; word_id < word_count; ++word_id)
-		// {
-		// 	for (size_t v = 0; v < max_letter; ++v)
-		// 		buf_blocks[v] += this->full_word_prefix_rank(data, word_id, v);
-
-		// 	// counted the values in the last word of the current block
-
-        //     // If last word in block
-		// 	if (word_id % words_per_block == (words_per_block - 1))
-		// 	{
-        //         // If not last block in superblock
-        //         if (word_id % words_per_superblock != (words_per_superblock - 1))
-        //         {
-        //             // If not at the end.
-        //             if (block_id < m_block.size()) // TODO: rewrite for loop to eliminate need for if clause here
-        //             {
-        //                 // assign currently buffered block counts per letter:
-        //                 for (uint64_t v = 0; v < max_letter; ++v)
-        //                     m_block[block_id + v] = buf_blocks[v];
-        //                 block_id += max_letter; // move to next block
-        //             }
-        //         }
-        //         else // In last block of superblock (note, we don't need the first block since it is always 0).
-        //         {   // If not at the end.
-        //             if (superblock_id < m_superblock.size()) // TODO: rewrite for loop to eliminate need for if clause here
-        //             {
-        //                 size_t superblock_text_id = (superblock_id / max_letter) - 1;
-        //                 size_t next_begin_position = superblock_text_id * values_per_superblock; // The next character to read from the text.
-        //                 size_t remaining_size = v->size() - next_begin_position; // The remaining size until the end of the text.
-
-        //                 // std::cout << "superblock_text_id = " << superblock_text_id << "\n";
-        //                 // std::cout << "text_begin = " << next_begin_position << "\n";
-        //                 // std::cout << "text_end = " << (next_begin_position + std::min<size_t>(remaining_size, values_per_superblock)) << "\n";
-
-        //                 auto text_end = std::next(text_it, values_per_superblock);
-        //                 // std::cout << "difference = " << text_end - text_it << "\n";
-        //                 m_compressed_text_per_superblock[superblock_text_id].assign(text_it, text_end);
-        //                 text_it = text_end;
-
-        //                 // For every letter accumulate the counts for the superblock.
-        //                 // Then store the cumulative sum in the respective superblock.
-        //                 // Then reset the block buffers.
-        //                 for (uint64_t v = 0; v < max_letter; ++v)
-        //                 {
-        //                     buf_superblocks[v] += buf_blocks[v];
-        //                     m_superblock[superblock_id + v] = buf_superblocks[v];
-        //                     buf_blocks[v] = 0; // reset blocks
-        //                 }
-        //             }
-        //             superblock_id += max_letter;
-        //         }
-		// 	}
-		// }
-        // m_compressed_text_per_superblock.back().assign(text_it, v->end());
-
-        // std::cout << "\nBlocks:\n";
-        // for (uint64_t i = 0; i < m_block.size(); i += max_letter)
-		// {
-		// 	for (uint64_t v = 0; v < max_letter; ++v)
-	    //         std::cout << (unsigned)m_block[i + v] << ' ';
-        //     std::cout << "| ";
-		// }
-        // std::cout << "\nSuperBlocks:\n";
-        // for (uint64_t i = 0; i < m_superblock.size(); i += max_letter)
-		// {
-		// 	for (uint64_t v = 0; v < max_letter; ++v)
-        //     	std::cout << (unsigned)m_superblock[i + v] << ' ';
-        //     std::cout << "| ";
-		// }
-        // std::cout << "\n\n";
-
-        //----------------------------------------------------------------------
-        // new initialisation
-        //----------------------------------------------------------------------
 
         // Allocate a node for every superblock.
         nodes.resize(superblock_count);
@@ -384,19 +290,8 @@ public:
 
         auto const & node = nodes[node_id];
 
-		// size_type const block_id_{idx / values_per_block};
-		// size_type const superblock_id_{block_id_ / blocks_per_superblock};
-        // size_type const block_id_in_superblock_{block_id_ - blocks_per_superblock * superblock_id_};
-      //size_type const block_id_in_superblock{block_id % blocks_per_superblock};
-    //   std::cout << "block in sb: " << block_id_in_superblock << " "
-    //             << ((idx / values_per_block) - blocks_per_superblock * ((idx / values_per_block) / blocks_per_superblock))
-    //             << "\n";
-
 		// retrieve superblock value
         size_type res = node.superblocks[v];
-        // std::cout << "res1 = " << res << " == " << m_superblock[max_letter * (block_id_ / blocks_per_superblock) + v] << '\n';
-		// std::cout << "res1=" << res << '\n';
-
 
 		// retrieve block value
         // The first block stores the counts for the actual second block.
@@ -404,14 +299,9 @@ public:
         // 0 (cache * node.block[pppp]). If the block id is greater than 0 we remove one from the letter
 		size_type const is_first_block = block_id_in_superblock == 0;
 		size_type const pppp = (max_letter * (block_id_in_superblock + is_first_block - 1)) + v;
-		// size_type const pppp = max_letter * (superblock_id * (blocks_per_superblock - 1) + (block_id_in_superblock + !cache - 1)) + v;
 		res += !is_first_block * (node.blocks[pppp]);
 
-        // if (block_id_in_superblock > 0)
-            // res += m_block[max_letter * superblock_id * (blocks_per_superblock - 1) + (block_id_in_superblock - 1)  * max_letter + v];
-
-        // size_t const pppp_ = max_letter * (superblock_id_ * (blocks_per_superblock - 1) + (block_id_in_superblock_ + is_first_block - 1)) + v;
-		// std::cout << "res2 = " << res << ": " << (!is_first_block * node.blocks[pppp]) << " == " << (!is_first_block * m_block[pppp_]) <<'\n';
+        // TODO: Enable me!
 		// compute in-block queries for all words before the in-block queries
 		// this only applies when multiple words are in one block
         // if constexpr (words_per_block > 1)
@@ -427,22 +317,14 @@ public:
         // }
 
 		// compute in-block query
-		// std::cout << "values_per_block=" << values_per_block << '\n';
-		// if (idx % values_per_block != 0)
-		// {
 		size_type cache2 = idx % values_per_block != 0;
 		// size_type const cache2 = 1;
         // First, get the local bit position within the data of the super block.
         size_type const bit_pos = (idx % values_per_superblock) * sigma_bits;
         // Second, compute the word that contains this value.
-        // uint64_t const word = *(node.compressed_text.data() + (bit_pos / this->bits_per_word));
-        uint64_t const word = *node.superblock_text[bit_pos / this->bits_per_word].data();
+        uint64_t const word = node.superblock_text[bit_pos / this->bits_per_word];
         // Third, compute the in-block rank given the current word.
         res += cache2 * this->word_prefix_rank_(word, bit_pos, v);
-        // res += cache2 * this->word_prefix_rank(this->m_v->data(), idx, v);
-			// std::cout << "idx=" << idx << '\n';
-		// }
-		// std::cout << "res4=" << res << '\n';
 
 		return res;
 	}
@@ -456,44 +338,22 @@ public:
 
 		constexpr uint8_t const max_letter{this->sigma - 1};
 
-		// size_type const block_id{idx / values_per_block};
-		// size_type const superblock_id{block_id / blocks_per_superblock};
-		// size_type const block_id_in_superblock{block_id - blocks_per_superblock * superblock_id};
-
         size_type const node_id = idx / values_per_superblock;
         size_type const block_id_in_superblock_ = (idx / values_per_block) % blocks_per_superblock;
         auto && node = nodes[node_id];
 
         // First access is the superblock_id offset with the max_letter plus the current letter.
-        // auto superblock_it = node.superblock_it + (v - 1); // Move iterator to the predecessor of v.
-
         size_t res_lower_ = node.superblocks[v - 1];
         size_t res_upper_ = node.superblocks[v];
 
-		// size_type const access1 = max_letter * superblock_id + v;  // Is the superblock id.
-
-		// auto it1 = m_superblock.begin() + (access1 - 1); // So we ask for the previous count which can be also from the other letter?
-		// size_type res_lower = *it1;
-		// ++it1;
-		// size_type res_upper = *it1;
-        // size_type res_upper = m_superblock[access1];
-        // size_type res_lower = m_superblock[access1 - 1];
-
 		size_type const is_first_block = block_id_in_superblock_ == 0;
-		// size_type const access2 = max_letter * (superblock_id * (blocks_per_superblock - 1) + (block_id_in_superblock + is_first_block - 1)) + v;
-		// // res_upper += is_first_block * m_block[access2];
-		// // res_lower += is_first_block * m_block[access2 - 1];
-		// auto it2 = m_block.begin() + (access2 - 1);
-		// res_lower += !is_first_block * (*it2);
-		// ++it2;
-		// res_upper += !is_first_block * (*it2);
 
         // Now we do the same with the blocks. We know that v is at least 1.
         size_type pppp = (max_letter * (block_id_in_superblock_ + is_first_block - 1)) + (v - 1);
         res_lower_ += !is_first_block * node.blocks[pppp];
         res_upper_ += !is_first_block * node.blocks[++pppp];
 
-
+        // TODO: Enable me!
         // if constexpr (words_per_block > 1)
         // {
         //     size_type const word_id{idx / values_per_word};
@@ -511,21 +371,10 @@ public:
         // First, get the local bit position within the data of the super block.
         size_type const bit_pos_ = (idx % values_per_superblock) * sigma_bits;
         // Second, compute the word that contains this value.
-        // uint64_t const word_ = *(node.text_slice.data() + (bit_pos_ / this->bits_per_word));
-        uint64_t const word_ = *node.superblock_text[bit_pos_ / this->bits_per_word].data();
+        uint64_t const word_ = node.superblock_text[bit_pos_ / this->bits_per_word];
         // Third, compute the in-block rank given the current word.
         res_lower_ += cache2_ * this->word_prefix_rank_(word_, bit_pos_, v - 1);
         res_upper_ += cache2_ * this->word_prefix_rank_(word_, bit_pos_, v);
-
-		// size_type const cache2 = idx % values_per_block != 0;
-		// size_type const bit_pos = (idx % values_per_superblock) * sigma_bits;
-        // // Second, compute the word that contains this value.
-        // uint64_t const word = *(m_compressed_text_per_superblock[superblock_id].data() + (bit_pos / this->bits_per_word));
-        // // Third, compute the in-block rank given the current word.
-        // res_upper += cache2 * this->word_prefix_rank_(word, bit_pos, v);
-        // res_lower += cache2 * this->word_prefix_rank_(word, bit_pos, v - 1);
-		// // res_upper += cache2 * this->word_prefix_rank(this->m_v->data(), idx, v);
-		// // res_lower += cache2 * this->word_prefix_rank(this->m_v->data(), idx, v - 1);
 
 		return res_upper_ - res_lower_;
 	}
