24ae32897b46b1ea2c5f5bbac4d8470a1d77ca47
rrahn <rene.rahn@fu-berlin.de>
2020-07-15 17:52:42 +0200

    [MISC] Remove epr subnamespace for rank benchmark and test.

--- sdsl/rank_benchmark.cpp
+++ sdsl/rank_benchmark.cpp
@@ -14,16 +14,16 @@ void rank(benchmark::State & state)
     uint8_t const log_sigma{static_cast<uint8_t>(std::clamp(std::ceil(std::log2(sigma)), 1.0, 64.0))};
     size_t const text_size{static_cast<size_t>(state.range(0))};
 
-    using size_type = typename sdsl::epr::rank_support_int_v<sigma>::size_type;
-    using value_type = typename sdsl::epr::rank_support_int_v<sigma>::value_type;
+    using size_type = typename sdsl::rank_support_int_v<sigma>::size_type;
+    using value_type = typename sdsl::rank_support_int_v<sigma>::value_type;
 
     std::mt19937_64 sigma_engine(12345);
     std::uniform_int_distribution<value_type> sigma_dist(0, sigma - 1);
     auto sigma_gen = [&sigma_dist, &sigma_engine]() { return sigma_dist(sigma_engine); };
 
-    sdsl::epr::int_vector<log_sigma> text(text_size, 0);
+    sdsl::int_vector<> text(text_size, 0);
     std::generate(text.begin(), text.end(), sigma_gen);
-    sdsl::epr::rank_support_int_v<sigma,log_sigma> rank_support(&text);
+    sdsl::rank_support_int_v<sigma> rank_support(&text);
 
     std::mt19937_64 position_engine(4654561);
     std::uniform_int_distribution<size_type> position_dist(0, text_size - 1);
@@ -37,9 +37,9 @@ void rank(benchmark::State & state)
     }
 }
 
-BENCHMARK_TEMPLATE(rank, 4)->RangeMultiplier(100)->Range(100, 1'000'000'000);
+BENCHMARK_TEMPLATE(rank, 4)->RangeMultiplier(100)->Range(100, 100'000'000);
 // BENCHMARK_TEMPLATE(rank, 4)->Arg(100000000);
-BENCHMARK_TEMPLATE(rank, 27)->RangeMultiplier(100)->Range(100, 1'000'000'000);
+BENCHMARK_TEMPLATE(rank, 27)->RangeMultiplier(100)->Range(100, 100'000'000);
 // BENCHMARK_TEMPLATE(rank, 8)->RangeMultiplier(100)->Range(100, 1'000'000);
 // BENCHMARK_TEMPLATE(rank, 16)->RangeMultiplier(100)->Range(100, 1'000'000);
 // BENCHMARK_TEMPLATE(rank, 32)->RangeMultiplier(100)->Range(100, 1'000'000);
--- sdsl/rank_test.cpp
+++ sdsl/rank_test.cpp
@@ -11,11 +11,11 @@ TEST(rank, crash)
     std::uniform_int_distribution<> dist(0, sigma - 1);
     auto gen = [&dist, &engine]() { return dist(engine); };
 
-    sdsl::epr::int_vector<> text(10'000, 0, 2);
+    sdsl::int_vector<> text(10'000, 0, 2);
     std::generate(text.begin(), text.end(), gen);
 
     std::vector<size_t> cnt_prefix_rank(sigma, 0);
-    sdsl::epr::rank_support_int_v<sigma> rank_vector(&text);
+    sdsl::rank_support_int_v<sigma> rank_vector(&text);
 
     rank_vector.rank(10'000, 3);
 }
@@ -28,11 +28,11 @@ TEST(rank, rankier)
     std::uniform_int_distribution<> dist(0, sigma - 1);
     auto gen = [&dist, &engine]() { return dist(engine); };
 
-    sdsl::epr::int_vector<5> text(500, 0, 5);
+    sdsl::int_vector<> text(500, 0, 5);
     std::generate(text.begin(), text.end(), gen);
 
     std::vector<size_t> cnt_prefix_rank(sigma, 0);
-    sdsl::epr::rank_support_int_v<sigma, 5> rank_vector(&text);
+    sdsl::rank_support_int_v<sigma> rank_vector(&text);
 
     for (size_t i = 0; i <= text.size(); ++i)
     {
